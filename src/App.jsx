3930500fa282a0e22976b013084216582e6b5c8d
Ravi prakash prajapati <raviprakashprajapati445@gmail.com>
2024-01-09 15:26:53 +0530

    Project File Structure Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import AdminLogin from "./components/admin/AdminLogin";
 import Home from "./components/public/Home";
-import Cart from "./components/user/Cart";
 import Login from "./components/user/Login";
 import Signup from "./components/user/Signup";
 import ProductDetailDesign from "./design/ProductDetailDesign";
@@ -13,7 +12,9 @@ function App() {
       {/* <Signup/> */}
       {/* <AdminLogin/> */}
       {/* <ProductDetailDesign/> */}
-      <Cart/>
+      
+      
+
 
     
 
